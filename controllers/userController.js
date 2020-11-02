bbfe49c30a3ca39048db23d3f2fc997829d39741
Hugo Hontans <hugohontans@gmail.com>
2020-11-02 15:30:42 +0100

    setup express server

---
+++ controllers/userController.js
@@ -0,0 +1,78 @@
+const User = require("../schema/schemaUser.js");
+const passwordHash = require("password-hash");
+
+async function signup(req, res) {
+  const { password, email } = req.body;
+  if (!email || !password) {
+    // Email or password null or undefined
+    return res.status(400).json({
+      text: "Invalid request"
+    });
+  }
+  // Creation of object User with hashed password
+  const user = {
+    email,
+    password: passwordHash.generate(password)
+  };
+  // Check if user already exists in DB
+  try {
+    const findUser = await User.findOne({
+      email
+    });
+    if (findUser) {
+      return res.status(400).json({
+        text: "The user already exists"
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+  try {
+    // Save user in DB
+    const userData = new User(user);
+    const userObject = await userData.save();
+    return res.status(200).json({
+      text: "Success",
+      email: userData.email,
+      token: userObject.getToken()
+    });
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+}
+
+async function login(req, res) {
+  const { password, email } = req.body;
+  if (!email || !password) {
+    // Email or password null or undefined
+    return res.status(400).json({
+      text: "Invalid request"
+    });
+  }
+  try {
+    // Check if user already exists in DB
+    const findUser = await User.findOne({ email });
+    if (!findUser)
+      return res.status(401).json({
+        text: "The user doesn't exist"
+      });
+    if (!findUser.authenticate(password))
+      return res.status(401).json({
+        text: "Invalid password"
+      });
+    return res.status(200).json({
+      token: findUser.getToken(),
+      email: findUser.email,
+      text: "Authentication successed"
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error
+    });
+  }
+}
+
+module.exports = function (app) {
+    app.post('/login', login);
+    app.post('/signup', signup);
+}
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.set('useCreateIndex', true);
+const bodyParser = require("body-parser");
+
+// Connexion to DB
+mongoose
+  .connect("mongodb://localhost/our-recipes", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((e) => {
+    console.log("Error while DB connecting");
+    console.log(e);
+  });
+
+const app = express();
+
+// Body Parser
+const urlencodedParser = bodyParser.urlencoded({
+  extended: true
+});
+app.use(urlencodedParser);
+
+app.use(bodyParser.json());
+
+// Definition of CORS
+app.use(function(req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+//Definition of router
+const router = express.Router();
+app.use("/ourrecipes/user", router);
+require(__dirname + "/controllers/userController")(router);
+
+// Definition of listening port
+const port = 8800;
+app.listen(port, () => console.log(`Listening on port ${port}`));
