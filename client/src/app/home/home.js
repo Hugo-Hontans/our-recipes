3786147ccc60349243b98332107a21b3bda856fd
Hugo Hontans <hugohontans@gmail.com>
2020-11-09 18:17:32 +0100

    delete recipes comments in home component

--- client/src/app/home/home.js
+++ client/src/app/home/home.js
@@ -4,29 +4,6 @@ import { RecipeCard } from '../recipe-card/recipe-card';
 import { CardColumns } from "react-bootstrap";
 import API from "../../utils/API";
 
-// const recipes = [
-//   {
-//     title: 'salade',
-//     tags: ['starter']
-//   },
-//   {
-//     title: 'soupe de choux',
-//     tags: ['starter', 'vege']
-//   },
-//   {
-//     title: 'salade',
-//     tags: ['dish']
-//   },
-//   {
-//     title: 'soupe de choux',
-//     tags: ['dish', 'vegan']
-//   },
-//   {
-//     title: 'salade',
-//     tags: ['dessert']
-//   },
-// ];
-
 const getAllRecipes = async () => {
   try {
     const { data } = await API.getAllRecipes();
@@ -50,7 +27,6 @@ export class Home extends React.Component {
   }
 
   render() {
-    console.log(this.state.recipes)
     return (
       <CardColumns>
         {this.state.recipes.map((recipe, index) => <RecipeCard key={index} recipe={recipe} index={index}></RecipeCard>)}
