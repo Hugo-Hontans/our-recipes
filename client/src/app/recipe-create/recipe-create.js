7cd3b46aa3ab4f6bbe606bec60088d5e33be1954
Hugo Hontans <hugohontans@gmail.com>
2020-11-11 21:40:58 +0100

    start to create new fields for a recipe

--- client/src/app/recipe-create/recipe-create.js
+++ client/src/app/recipe-create/recipe-create.js
@@ -9,13 +9,16 @@ const tagLabels = ['Starter', 'Dish', 'Dessert', 'Drink', 'Accompaniment', 'Vege
 export const RecipeCreate = () => {
   const [title, setTitle] = useState('');
   const [tags, setTags] = useState([]);
+  const [preparingTime, setPreparingTime] = useState('');
+  const [bakingTime, setBakingTime] = useState('');
+  const [people, setPeople] = useState('');
+  const [difficulty, setDifficulty] = useState('');
+  const [cheap, setCheap] = useState('');
+  const [quote, setQuote] = useState('');
+
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isInError, setIsInError] = useState(false);
   const [isSuccessed, setIsSuccessed] = useState(false);
-
-  const handleChangeTitle = (event) => {
-    setTitle(event.target.value);
-  };
 
   const handleChangeTag = (event) => {
     const value = event.target.value;
@@ -50,7 +53,7 @@ export const RecipeCreate = () => {
     <Form onSubmit={(event) => {create(); event.preventDefault();}}>
       <Form.Group controlId="title">
         <Form.Label>Title</Form.Label>
-        <Form.Control value={title} onChange={handleChangeTitle} type="text" placeholder="Title" />
+        <Form.Control value={title} onChange={event => setTitle(event.target.value)} type="text" placeholder="Title" />
       </Form.Group>
 
       <Form.Group controlId="tags">
@@ -67,6 +70,33 @@ export const RecipeCreate = () => {
         <Form.Text className="text-muted">
           Choose one or mutiple tags.
         </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="time">
+        <Form.Label>Preparing Time (minutes)</Form.Label>
+        <Form.Control value={preparingTime} onChange={event => setPreparingTime(event.target.value)} type="number" placeholder="Time to prepare" />
+        <Form.Label>Baking Time (minutes)</Form.Label>
+        <Form.Control value={bakingTime} onChange={event => setBakingTime(event.target.value)} type="number" placeholder="Time to bake" />
+      </Form.Group>
+
+      <Form.Group controlId="people">
+        <Form.Label>People</Form.Label>
+        <Form.Control value={people} onChange={event => setPeople(event.target.value)} type="number" placeholder="Number of people" />
+      </Form.Group>
+
+      <Form.Group controlId="difficulty">
+        <Form.Label>Difficulty (1 to 3)</Form.Label>
+        <Form.Control value={difficulty} onChange={event => setDifficulty(event.target.value)} type="number" placeholder="Difficulty" min="1" max="3" />
+      </Form.Group>
+
+      <Form.Group controlId="cheap">
+        <Form.Label>Cheap (1 to 3)</Form.Label>
+        <Form.Control value={cheap} onChange={event => setCheap(event.target.value)} type="number" placeholder="Cheap" min="1" max="3" />
+      </Form.Group>
+
+      <Form.Group controlId="quote">
+        <Form.Label>My quote</Form.Label>
+        <Form.Control value={quote} onChange={event => setQuote(event.target.value)} type="text" placeholder="To taste with..." />
       </Form.Group>
 
       <Button variant="primary" type="submit" disabled={isSubmitting}>
