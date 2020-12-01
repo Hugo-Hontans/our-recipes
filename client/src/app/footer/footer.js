5fdf0a01715a25d4c8e958f8a017bae828d45c29
Hugo Hontans <hugohontans@gmail.com>
2020-12-01 17:04:39 +0100

    change footer links

--- client/src/app/footer/footer.js
+++ client/src/app/footer/footer.js
@@ -4,10 +4,8 @@ import "./footer.css";
 export const Footer = () =>
   <article className="footer bg-dark">
     <div className="footer-group">
-      Icons made by <a href="https://www.flaticon.com/authors/freepik" target="_blank" rel="noopener noreferrer" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer" title="Flaticon">www.flaticon.com</a>
+      Icons made by <a href="https://www.flaticon.com/authors/freepik" target="_blank" rel="noopener noreferrer" title="Freepik">Freepik</a>, <a href="https://www.flaticon.com/authors/gregor-cresnar" target="_blank" rel="noopener noreferrer" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer" title="Flaticon">www.flaticon.com</a>
       <br />
       Fond photo créé par topntp26 -<a href='https://fr.freepik.com/photos/fond' target="_blank" rel="noopener noreferrer">fr.freepik.com</a>
-      <br />
-      Icons made by <a href="https://www.flaticon.com/authors/gregor-cresnar" target="_blank" rel="noopener noreferrer" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="https://www.flaticon.com/" target="_blank" rel="noopener noreferrer" title="Flaticon"> www.flaticon.com</a>
     </div>
   </article>
